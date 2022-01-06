5cad0158de171a8691727c7cf1f9d0b57ffbf626
darshit <darshitjain126@gmail.com>
2022-01-06 18:39:55 +0530

    signin in progress

---
+++ register/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+#from django.contrib.auth import login, authenticate
+#from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
+
+#Create your views here.
+def register(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)   
+        if form.is_valid():
+            form.save()
+
+        return redirect("/contact")
+    else:
+        form = RegisterForm()
+
+    return render(response, "register/register.html", {"form":form})
+"""
+def register(response):
+    if response.method == "POST":
+	    form = RegisterForm(response.POST)
+
+        if form.is_valid():
+	        form.save()
+	    return redirect("/")
+    else:
+	    form = RegisterForm()
+    return render(response, "register/register.html", {"form":form})
+    
+"""
